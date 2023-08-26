dd9246081f8ed25cb9345635f0c0e2c74f8f7a96
JaysinhGo <jaysinh.go@gmail.com>
2023-08-26 15:07:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
+// Define an array of URLs for making API calls
 const urls = [
-    "https://pokeapi.co/api/v2/type/-1", // 404 
-    "https://pokeapi.co/api/v2/type/0", // 404
-    "https://pokeapi.co/api/v2/type/1",
-    "https://pokeapi.co/api/v2/type/2",
-  ];
-  
-  const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-  
-  const handleFailedRequest = async (url, index) => {
+    "https://pokeapi.co/api/v2/type/-1", // Invalid type, will result in a 404
+    "https://pokeapi.co/api/v2/type/0",  // Another invalid type, will result in a 404
+    "https://pokeapi.co/api/v2/type/1",  // URL for type 1
+    "https://pokeapi.co/api/v2/type/2",  // URL for type 2
+];
+
+// Define a function to introduce a delay using Promises
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+// Function to handle a failed API request
+const handleFailedRequest = async (url, index) => {
     console.log(`API call ${url} failed, moving to the end of the array`);
+    // Move the failed URL to the end of the array
     urls.push(urls.splice(index, 1)[0]);
+    // Introduce a delay of 1 second before retrying
     await delay(1000);
-  };
-  
-  const makeAPICalls = async () => {
+};
+
+// Main function for making API calls
+const makeAPICalls = async () => {
     for (let i = 0; i < urls.length; i++) {
-      const url = urls[i];
-  
-      try {
-        const response = await fetch(url);
-        if (response.ok) {
-          console.log(`API call ${url} successful`);
-          await delay(1000);
-        } else {
-          await handleFailedRequest(url, i);
-          i--;
+        const url = urls[i];
+
+        try {
+            // Attempt to fetch data from the current URL
+            const response = await fetch(url);
+            if (response.ok) {
+                console.log(`API call ${url} successful`);
+                // Introduce a delay of 1 second before making the next call
+                await delay(1000);
+            } else {
+                // If the response is not OK, handle the failed request
+                await handleFailedRequest(url, i);
+                i--; // Decrement i to retry the same URL in the next iteration
+            }
+        } catch (error) {
+            console.error(`An error occurred while calling ${url}: ${error}`);
+            // If an error occurs during the API call, handle the failed request
+            await handleFailedRequest(url, i);
+            i--; // Decrement i to retry the same URL in the next iteration
         }
-      } catch (error) {
-        console.error(`An error occurred while calling ${url}: ${error}`);
-        await handleFailedRequest(url, i);
-        i--;
-      }
     }
-  };
-  makeAPICalls();
+};
+
+// Initiate the API calling process
+makeAPICalls();
+
+  
